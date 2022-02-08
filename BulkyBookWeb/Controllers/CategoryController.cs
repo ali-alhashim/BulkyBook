cd208320c58d56965529da8bf9e80abdbb348ce0
ALI MUSA ALHASHIM <93006102+ali-alhashim@users.noreply.github.com>
2022-02-08 22:24:33 +0300

    CREATE

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBookWeb.Controllers
@@ -15,11 +16,26 @@ namespace BulkyBookWeb.Controllers
 
         public IActionResult Index()
         {
-            var objCategoryList = _db.Categories.ToList();
+            IEnumerable<Category> objCategoryList = _db.Categories;
 
            
 
+            return View(objCategoryList);
+        }
+
+
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+             _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
